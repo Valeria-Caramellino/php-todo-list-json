75d64cd92a1af8298147c9760116620eddcd3a86
valeria caramellino <valeriacaramellino@gmail.com>
2023-06-12 15:47:45 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP + VUE</title>
+     <!--bootstrap-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+   <link rel="stylesheet" href="style.css">
+
+   <!--vue-->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+    <!--axios-->
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+</head>
+<body>
+    
+    <div id="app" class="text-center">
+
+        <header>
+            <section class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h1 :class="riprendoClasse"> {{ titolo }} </h1>
+                    </div>
+                </div>
+
+            </section>
+        </header>
+        <main>
+            <section class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <img class="rounded-circle" :src="img" alt="mare">
+                    </div>
+<!--
+                    <div class="col-12">
+                        <button @click="chiamata" class="btn btn-success my-3">
+                            click
+                        </button>
+-->
+                    </div>
+
+                </div>
+                <div class="row">
+                    <div class="col-4">
+                        <p>
+                            {{ ArrayStudenti[0].name }}
+                        </p>
+                    </div>
+                
+                </div>
+
+            </section>
+            
+        </main>
+    
+    </div>
+    <script src="./main.js"></script>
+
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,32 @@
+const { createApp } = Vue;
+createApp({
+    data(){
+        return{
+            titolo: "Ben Tornato Vue!",
+            img :"https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
+            riprendoClasse:"green",
+            api:"api.php",
+            ArrayStudenti:[],
+        }
+    },
+    methods:{
+       /* 
+        chiamata(){
+            axios.get(this.api).then((risposta) => {
+                console.log("Dati ricevuti: ", risposta.data);
+            });
+        },
+        */
+    },
+    mounted(){
+        console.log("Buon giorno vue!");
+
+        axios.get(this.api).then((oggetto) => {
+            console.log("Dati ricevuti: ", oggetto.data);
+            const risposta = oggetto.data;
+            this.ArrayStudenti = risposta;
+            console.log("array è", this.ArrayStudenti)
+        });
+        
+    }
+}).mount("#app");
